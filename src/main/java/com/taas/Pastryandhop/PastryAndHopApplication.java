ebbc91a15e0aeb8d6531ade7a1e806f7a3f451be
Giorgio Mecca <g.mecca@reply.it>
2022-12-28 19:04:34 +0100

    init

---
+++ src/main/java/com/taas/Pastryandhop/PastryAndHopApplication.java
@@ -0,0 +1,13 @@
+package com.taas.Pastryandhop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastryAndHopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastryAndHopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taas/Pastryandhop/PastryAndHopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taas.Pastryandhop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastryAndHopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
